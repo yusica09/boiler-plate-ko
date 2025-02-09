cde3993deabe91c231e6297199b09cb795665e49
yusica09 <choijiwoo0922@gmail.com>
2025-02-09 21:18:56 +0900

    정보 보호

---
+++ config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+  //MONGO_URI 부분은 HEROKU에서랑 똑같이 해야함 #9 참고
+  mongoURI: process.env.MONGO_URI,
+};
--- index.js
+++ index.js
@@ -2,21 +2,53 @@
 const express = require("express");
 const app = express();
 const port = 5001;
+const bodyParser = require("body-parser");
+
+const config = require("./config/key");
+
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/jsom
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://jiwoo:abcd1234@boilerplate.oyfps.mongodb.net/?retryWrites=true&w=majority&appName=boilerplate",
-    {
-      //useNewUrlParser: true,
-      //useUnifiedTopology: true,
-      //useCreateIndex: true,
-      //useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    //useNewUrlParser: true,
+    //useUnifiedTopology: true,
+    //useCreateIndex: true,
+    //useFindAndModify: false,
+  })
   .then(() => console.log("MongoDB Connected.."))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World! ~안녕하세요~"));
+app.get("/", (req, res) => res.send("Hello World!~ 업데이트"));
+
+app.post("/register", (req, res) => {
+  // 회원가입 할때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다
+
+  const user = new User(req.body);
+
+  //   user.save((err, userInfo) => {
+  //     if (err) return res.json({ success: false, err });
+  //     return res.status(200).json({
+  //       success: true,
+  //     });
+  //   });
+  user
+    .save()
+    .then((userInfo) => {
+      return res.status(200).json({
+        success: true,
+      });
+    })
+    .catch((err) => {
+      return res.json({ success: false, err });
+    });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!"`));
